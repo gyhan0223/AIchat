db27dafa5a89b3cb38a5e47ed58d153482f7315c
gyhan0223 <gyhan21@icloud.com>
2025-05-22 18:00:27 +0900

    정리 다 됨

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
-// App.js
 import React from "react";
+import { SafeAreaView } from "react-native";
 import ChatScreen from "./screens/ChatScreen";
 
 export default function App() {
-  return <ChatScreen />;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ChatScreen />
+    </SafeAreaView>
+  );
 }
